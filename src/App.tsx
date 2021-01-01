d0f4528c8818cb349c48f415da04388600c4adc5
pal4de <pal4de@gmail.com>
2021-01-01 23:53:15 +0900

    プロフィール画像を作ってみる

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.scss';
+import ProfileImageExample from './resource/nadegata_2.png';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello Wolrd</p>
+      <ProfileImage src={ProfileImageExample} />
     </div>
   );
+}
+
+const ProfileImage: React.FC<{src: string}> = (props) => {
+  return <img className="ProfileImage" src={props.src} alt="プロフィール画像" />;
 }
 
 export default App;
