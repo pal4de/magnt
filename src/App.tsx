c9fedbdbb532f608e98fdcbc54ee86261e374f17
pal4de <pal4de@gmail.com>
2021-01-02 00:50:49 +0900

    ユーザー名やbioを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.scss';
-import ProfileImageExample from './resource/nadegata_2.png';
+// import profileImageExample from './resource/nadegata_2.png';
+
+const exampleProfile: ProfileProps = {
+  name: "pal4de",
+  image: "example/profile_image.png",
+  bio: "hello there",
+}
 
 function App() {
   return (
     <div className="App">
-      <ProfileImage src={ProfileImageExample} />
+      <Profile data={exampleProfile} />
     </div>
   );
 }
 
-const ProfileImage: React.FC<{src: string}> = (props) => {
-  return <img className="ProfileImage" src={props.src} alt="プロフィール画像" />;
-}
+type ProfileProps = {
+  name: string,
+  image: string,
+  bio?: string,
+};
+const Profile: FC<{data: ProfileProps}> = (props) => {
+  return (
+    <header className="Profile">
+      <ProfileImage src={props.data.image} />
+      <ProfileName name={props.data.name} />
+      {props.data.bio && <ProfileBio bio={props.data.bio} />}
+    </header>
+  );
+};
+
+const ProfileImage: FC<{src: string}> = (props) => {
+  const style: React.CSSProperties = {
+    backgroundImage: `url(${props.src})`
+  };
+  return <div className="ProfileImage" style={style} />;
+};
+
+const ProfileName: FC<{name: string}> = (props) => {
+  return <h3 className="ProfileName">{props.name}</h3>;
+};
+
+const ProfileBio: FC<{bio: string}> = (props) => {
+  return <div className="ProfileBio">{props.bio}</div>;
+};
 
 export default App;
